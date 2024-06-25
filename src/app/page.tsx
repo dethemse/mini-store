3a35e95adddc720c1942d906f4c278b00bb65e9f
DThemse <de.themse@gmail.com>
2024-06-25 21:19:52 +0300

    feat: button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import 'server-only';
 
 import { PageProps } from '@/types/app';
+import { Button } from '@/ui/components/atoms/Button';
 
 export default function Home({}: PageProps) {
 	return (
 		<main className="p-24">
 			<h1>Hello World</h1>
-			<p>Hello World</p>
+			<Button>Lorem</Button>
 		</main>
 	);
 }
