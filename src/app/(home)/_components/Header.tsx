73d0ce74a3c17e0962b93b4610859f6316415fc7
DThemse <de.themse@gmail.com>
2024-06-26 04:54:41 +0300

    feat: cart button

--- src/app/(home)/_components/Header.tsx
+++ src/app/(home)/_components/Header.tsx
@@ -1,8 +1,18 @@
 import Link from 'next/link';
 
 import { Brand } from '@/ui/components/organisms/Brand';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+	PopoverArrow,
+} from '@/ui/components/molecules/Popover';
+import { ShoppingBasketIcon } from '@/ui/icons';
+import { ProductCart } from '@/ui/components/organisms/ProductCart';
 
 export const Header = () => {
+	const cartCounter = 0;
+
 	return (
 		<div className="bg-slate-600">
 			<div className="container flex min-h-16 items-center justify-between">
@@ -10,7 +20,17 @@ export const Header = () => {
 					<Brand />
 				</Link>
 
-				<div>Cart</div>
+				<Popover>
+					<PopoverTrigger className="flex gap-x-1 text-white">
+						<ShoppingBasketIcon className="size-6" />
+						<span>({cartCounter})</span>
+					</PopoverTrigger>
+					<PopoverContent align="end" sideOffset={10}>
+						{cartCounter > 0 ? <ProductCart /> : <p className="text-center">Empty</p>}
+
+						<PopoverArrow className="fill-white" />
+					</PopoverContent>
+				</Popover>
 			</div>
 		</div>
 	);
---
+++ src/ui/icons/index.ts
@@ -0,0 +1,1 @@
+export { ShoppingBasketIcon } from 'lucide-react';
