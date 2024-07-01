754473cb5f43cc2f697eb4e1ff04bc7c76aa196f
DThemse <de.themse@gmail.com>
2024-07-01 08:02:27 +0300

    feat: separator

---
+++ src/ui/components/atoms/Separator/Separator.tsx
@@ -0,0 +1,5 @@
+export const Separator = () => {
+	return (
+		<div className="mx-auto my-4 h-1 w-full max-w-6xl bg-[linear-gradient(90deg,rgba(149,131,198,0)_1.46%,rgba(149,131,198,0.6)_40.83%,rgba(149,131,198,0.3)_65.57%,rgba(149,131,198,0)_107.92%)]" />
+	);
+};
